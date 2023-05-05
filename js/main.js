1cde3081f0ecda28f86307f9461d24e5753c291e
j-vaz12 <jasonvasquezx@gmail.com>
2023-05-05 15:52:50 -0700

    setup

---
+++ js/main.js
@@ -0,0 +1,15 @@
+	/*----- constants -----*/
+
+
+	/*----- state variables -----*/
+	let secretWord;
+	let results;
+	let correctGuess;
+	
+	/*----- cached elements  -----*/
+
+
+	/*----- event listeners -----*/
+
+
+	/*----- functions -----*/
