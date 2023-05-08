d9540a853ce1458631bdde99faec43ea0120b224
j-vaz12 <jasonvasquezx@gmail.com>
2023-05-08 11:50:29 -0700

    renderResults function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 /*----- constants -----*/
 const Words = ['MOON', 'NOVA', 'PLANET', 'ROCKET'];
 const MAX_WRONG_GUESSES = 6;
-
+const SPACEMAN_LOOKUP = {
+	
+}
 /*----- state variables -----*/
 /*let result;   correct word? Maybe this will come ones I start with the functions   */
 let correctWord;
 let playerWord; 
 let wrongGuesses; 
-let wiiner;
-/* also as player letter?   */
-// let correctGuess;  dont think I needs this if I have the const above 
+let winner;
+
+// let correctGuess;  look at what jim said in the gc
 
 /*----- cached elements  -----*/
 const imgResultsEl = document.getElementById('game-result');
@@ -22,7 +24,8 @@ document.querySelector('main').addEventListener('click', handleEvent)
 initialize();
 
 function handleEvent(evt) {
-	// evt.target = 'main'	
+	//my guard so that it will on handle evt when clicking in a button 
+	if (evt.target.tagName !== 'BUTTON') return; 
 	console.log(evt);
 
 	render();
@@ -30,14 +33,31 @@ function handleEvent(evt) {
 function initialize() {
 	correctWord = null;
 	playerWord = '';
-	wrongGuesses = null;
-	wiiner = null;
+	wrongGuesses = 0;
+	winner = playerWord;
 	render();
 }
 
+// Remember that the render is to visuualoze everyhing or all state om the DOM
 function render() {
-	// renderResults();
-	// renderButton();
+	renderResults();
+
+	/*  to toggle the play again button also to make the apphabet  buttons un clocalbe */
+	renderButton();
+}
+ 
+function renderResults() {
+	if (playerWord === correctWord) {
+		return winner; 
+	} else if (playerWord !== correctWord) {
+		return wrongGuesses++
+	// } else if (wrongGuesses === MAX_WRONG_GUESSES) {
+		 
+	// } I think i can add this on my evthanndle 
+}
+
+function renderButton() {
+
 }
 
 // const imgPath = imgs/spaceman-0${wrongGuesses.length};
