21a017deb6e2e13f3530e51041d83451d7af15c9
j-vaz12 <jasonvasquezx@gmail.com>
2023-05-07 17:50:40 -0700

    cahced elemnts/ function start

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 /*----- constants -----*/
 const allWords = ['Moon', 'Nova', 'Planet', 'Rocket']
-
+const MAX_WRONG_GUESSES = 6
 
 /*----- state variables -----*/
-let secretWord;
-let result;
-let correctGuess;
-let playerWord;
+let result; /*  correct word? Maybe this will come ones I start with the functions   */
+let correctWord;
+let playerWord; /* also as player letter?   */
+// let correctGuess;  dont think I needs this if I have the const above 
 
 /*----- cached elements  -----*/
-
-
+const imgResultsEl = document.getElementById('game-result')
+const guessesRemain = document.querySelector('p')
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
+
+
+function initialize() {
+
+result = {
+	pGuess: " ",
+	correctWord: ""
+
+};
+
+
+	render()
+}
+
+function render() {
+	renderResults();
+
+}
