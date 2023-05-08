cb4f7252b31feb70c26b12282d580d6309d398b1
j-vaz12 <jasonvasquezx@gmail.com>
2023-05-08 10:49:53 -0700

    init function and evt listener

--- js/main.js
+++ js/main.js
@@ -1,35 +1,43 @@
 /*----- constants -----*/
-const allWords = ['Moon', 'Nova', 'Planet', 'Rocket']
-const MAX_WRONG_GUESSES = 6
+const Words = ['MOON', 'NOVA', 'PLANET', 'ROCKET'];
+const MAX_WRONG_GUESSES = 6;
 
 /*----- state variables -----*/
-let result; /*  correct word? Maybe this will come ones I start with the functions   */
+/*let result;   correct word? Maybe this will come ones I start with the functions   */
 let correctWord;
-let playerWord; /* also as player letter?   */
+let playerWord; 
+let wrongGuesses; 
+let wiiner;
+/* also as player letter?   */
 // let correctGuess;  dont think I needs this if I have the const above 
 
 /*----- cached elements  -----*/
-const imgResultsEl = document.getElementById('game-result')
-const guessesRemain = document.querySelector('p')
-/*----- event listeners -----*/
+const imgResultsEl = document.getElementById('game-result');
+const guessesRemain = document.querySelector('p');
 
+/*----- event listeners -----*/
+document.querySelector('main').addEventListener('click', handleEvent)
 
 /*----- functions -----*/
+initialize();
 
+function handleEvent(evt) {
+	// evt.target = 'main'	
+	console.log(evt);
 
+	render();
+}
 function initialize() {
-
-result = {
-	pGuess: " ",
-	correctWord: ""
-
-};
-
-
-	render()
+	correctWord = null;
+	playerWord = '';
+	wrongGuesses = null;
+	wiiner = null;
+	render();
 }
 
 function render() {
-	renderResults();
-
+	// renderResults();
+	// renderButton();
 }
+
+// const imgPath = imgs/spaceman-0${wrongGuesses.length};
