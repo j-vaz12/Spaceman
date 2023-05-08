2a1397be34e1ac5d12557d7045437bbdf4862efc
j-vaz12 <jasonvasquezx@gmail.com>
2023-05-08 16:18:32 -0700

    new function

--- js/main.js
+++ js/main.js
@@ -1,77 +1,83 @@
 /*----- constants -----*/
-const Words = ['MOON', 'NOVA', 'PLANET', 'ROCKET'];
+const WORDS = ['MOON', 'NOVA', 'PLANET', 'ROCKET'];
 const MAX_WRONG_GUESSES = 6;
-const imgPath = imgs/spaceman-0${wrongGuesses.length};
+// const imgPath = imgs/spaceman-0${wrongGuesses.length};
 
 /*----- state variables -----*/
 /*let result;   correct word? Maybe this will come ones I start with the functions   */
 let correctWord;
-let playerWord; 
-let wrongGuesses; 
+let playerWord;
+let wrongGuesses;
 let winner;
 
 // let correctGuess;  look at what jim said in the gc
 
 /*----- cached elements  -----*/
 const imgResultsEl = document.getElementById('game-result');
-const guessesRemain = document.querySelector('p');
-
+const guessesRemainEl = document.querySelector('p');
+const rightLetterEl = document.querySelector('div')
 /*----- event listeners -----*/
-document.querySelector('main').addEventListener('click', handleEvent)
+document.querySelector('main').addEventListener('click', handleLetterClick);
 
 /*----- functions -----*/
 initialize();
 
-function handleEvent(evt) {
+function handleLetterClick(evt) {
 	//my guard so that it will on handle evt when clicking in a button 
-	if (evt.target.tagName !== 'BUTTON') return; 
-	console.log(evt);
-
+	if (evt.target.tagName !== 'BUTTON') return;
+	// I need to make it so that the letter variable is set to whatever button the player picks
+	const letter = evt.target.innerText;
+	if (correctWord.includes(letter)) {
+		return correctLtr();
+	} else {
+		wrongGuesses++
+		console.log(wrongGuesses)
+	}
 	render();
 }
 function initialize() {
-	correctWord = null;
+	correctWord = WORDS[Math.floor(Math.random() * WORDS.length)];
 	playerWord = '';
 	wrongGuesses = 0;
 	winner = null;
 	render();
 }
 
-// Remember that the render is to visuualoze everyhing or all state om the DOM
+function correctLtr() {
+	for (let i = 0; i < correctWord.length; i++) {
+		if (playerWord === correctWord[i]) {
+			return playerWord += correctWord
+		} else
+			break
+	}
+}
+
+// function putLetter(letter) {
+// 	let updatedLetter = '';
+// 	for (let i = 0; i < correctWord.length; i++) {
+// 		if (correctWord[i] === letter) {
+// 			updatedLetter = updatedLetter + letter;
+// 		} else {
+// 			updatedLetter = updatedLetter + playerWord
+// 		}
+// 	}
+// 	playerWord = updatedLetter;
+// }
+
+// Remember that the render is to vizualizing everything or all state om the DOM
 function render() {
 	renderResults();
-
 	/*  to toggle the play again button also to make the apphabet  buttons un clocalbe */
 	renderButton();
 }
-function whatIsCorrectWord() {
-// if (playerWord === correctWord) {
-// 		return winner; 
-// 	} else if (playerWord !== correctWord) {
-// 		return wrongGuesses++
-// 	} else if()
-// 	} else if (wrongGuesses === MAX_WRONG_GUESSES) {
-		 
-	// } I think i can add this on my evthanndle 
-}
-
-function whatIsPlayerWord() {
-
-}
-
-function calculateGuesses() {
-
-}
-
-function getWinner() {
-
-}
-
+// function render 
 function renderResults() {
+	rightLetterEl.innerText = playerWord;
+
 }
 
 function renderButton() {
 
 }
-
 // const imgPath = imgs/spaceman-0${wrongGuesses.length};
+
