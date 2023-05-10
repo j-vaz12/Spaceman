fb1321fb80d8e86ae19f93b2f6b84e683c3e6d1e
j-vaz12 <jasonvasquezx@gmail.com>
2023-05-10 16:29:17 -0700

    btn render logic pt2

--- js/main.js
+++ js/main.js
@@ -81,30 +81,32 @@ function renderSpaceMan() {
 	imgResultsEl.src = `spaceman-images/spaceman-${wrongGuesses.length}.jpg`
 }
 
-function renderResults() {  
+function renderResults() {
 	// todo if game satus is === W change winresults text content to "YOU WIN!!"
 	// if game satus is === L change winf result to text content `YOU LOSE the correct word is {correct.join("")}`
 	// else change text content to maxwrong - wronggueses.length 
-	if (winner ===  "W") {  
+	if (winner === "W") {
 		winResultEl.innerText = "You Win!!!";
 	} else if (winner === "L") {
-		winResultEl.innerText= "YOU LOST HAHAHA!!!";
+		winResultEl.innerText = "YOU LOST HAHAHA!!!";
 	} else {
 		winResultEl.innerText = `Guesses Remaining: ${MAX_WRONG_GUESSES - wrongGuesses.length}`;
 	}
 }
 
 function renderButton() {
-//todo cache the elment buttons "main > button" forEach that element 
-	allBtn.forEach(function(btn, idx) {
-		if(btn.innerText === letter) {
+	//todo cache the elment buttons "main > button" forEach that element 
+	allBtn.forEach(function (btn, idx) {
+		if (btn.innerText ===  /*  letter is not in scope bc its not global   */) {
 			btn.classList.toggle('right')
+		} else if () {
+			tf
 		}
 	});
-// letter = button.tectContent or run check
-//if wrongguess.included letter change bttuon class name to wrong
-// else if word status includes letter change the button class to correct
-// else button.class name is epmepty string
+	// letter = button.tectContent or run check
+	//if wrongGuess.included letter change bttuon class name to wrong
+	// else if word status includes letter change the button class to correct
+	// else button.class name is epmepty string
 }
 
 
